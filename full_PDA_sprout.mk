2a50ff8ebd9008154dde658ec777e3c1755936ab
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2020-05-19 15:18:58 +0500

    Delete full_PDA_sprout.mk

--- full_PDA_sprout.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-PRODUCT_DEVICE := PDA_sprout
-PRODUCT_NAME := omni_PDA_sprout
-PRODUCT_BRAND := Nokia
-PRODUCT_MODEL := PDA_sprout
-PRODUCT_MANUFACTURER := Nokia
-PRODUCT_BOARD := mt6771
